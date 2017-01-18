ff6fdf88379b86c7be77ff5bc0cf9938e481c552
rgdelato <rgdelato@gmail.com>
2017-01-17 16:31:58 -0800

    testing precommit with if statement

--- precommit-filter.js
+++ precommit-filter.js
@@ -2,7 +2,7 @@
 var staged_files = process.argv.slice(2);
 
 var staged_js_files = staged_files.filter(filename => {
-  return filename.endsWith(".js");
+  return filename.startsWith("src/") && filename.endsWith(".js");
 });
 
 console.log(staged_js_files.join(" "));
